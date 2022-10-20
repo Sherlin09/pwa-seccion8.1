a3b9b794256fe9e5d4c73cc5f2a464a2ef5f87a4
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-10-20 17:46:14 -0500

    90 manejo de errores e insercion de datos

--- app.js
+++ app.js
@@ -7,4 +7,42 @@ request.onupgradeneeded = event => {
     db.createObjectStore('heroes', {
         keyPath: 'id'
     });
-}
+};
+
+//manejo de erroees
+request.onerror = event => {
+    console.log('DB error:', event.target.error);
+};
+
+//insertar datos
+request.onsuccess = event => {
+    let db = event.target.result;
+
+    let heroesData = [
+        { id: '1111', heroe: 'Spiderman', mensaje: 'holi' },
+        { id: '2222', heroe: 'Ironman <3', mensaje: 'holi bb' }
+    ];
+
+    let heroesTransaction = db.transaction('heroes', 'readwrite');
+
+    heroesTransaction.onerror = event => {
+        console.log('Error guardaddo', event.target.error);
+    };
+
+    //exito de la transaccion
+
+    heroesTransaction.oncomplete = event => {
+        console.log('Transaccion hecha', event);
+    };
+
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+
+    for (let heroe of heroesData) {
+        heroesStore.add(heroe);
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado a la DB');
+    }
+};
