f46582dcead6ebc268d99f1d9c31e2c5500b576c
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-10-20 15:46:46 -0500

    89 inicios de indexDB

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-
 // indexedDB: Reforzamiento
+let request = window.indexedDB.open('mi-database', 1);
 
-
-
-
+request.onupgradeneeded = event => {
+    console.log('Actializacion');
+    let db = event.target.result;
+    db.createObjectStore('heroes', {
+        keyPath: 'id'
+    });
+}
